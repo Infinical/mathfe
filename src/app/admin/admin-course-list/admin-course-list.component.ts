954c65105b805a744ce3252aa44ac3c34fffeab8
destinato <nikitos290@gmail.com>
2018-10-16 18:40:22 +0300

    sort courses

--- src/app/admin/admin-course-list/admin-course-list.component.ts
+++ src/app/admin/admin-course-list/admin-course-list.component.ts
@@ -9,14 +9,24 @@ import { Course } from '../../models/course';
   styleUrls: ['./admin-course-list.component.css']
 })
 export class AdminCourseListComponent implements OnInit {
-  beURL = "http://localhost:8000/";
+  beURL = window.location.origin + '/';
 
-  courses: Observable<Course[]>;
+  courses: Course[];
+
+  // sort block
+
+  public sortedByTitle: boolean = false;
+  public sortedByDescription: boolean = false;
+  public sortedByStart: boolean = false;
+  public sortedByEnd: boolean = false;
+
+  // end sort block
 
   constructor(private courseService: CourseService) { }
 
   ngOnInit() {
-    this.courses = this.courseService.getCourses();
+    this.courseService.getCourses()
+      .subscribe(items => this.courses = items.sort(this._sortById));
   }
 
   resetUpdateStatus() {
@@ -26,5 +36,92 @@ export class AdminCourseListComponent implements OnInit {
   get updateStatus(): string {
     return this.courseService.updateStatus;
   }
+
+  // sort block
+
+  public sortBy(str: string): void {
+    if (this.courses && this.courses.length) {
+      switch (str) {
+        case 'title':
+          this.sortedByTitle ? this.courses.reverse() : this.courses.sort(this._sortByTitle); 
+          this.sortedByTitle = !this.sortedByTitle;
+          break;
+        case 'description':
+          this.sortedByDescription ? this.courses.reverse() : this.courses.sort(this._sortByDescription); 
+          this.sortedByDescription = !this.sortedByDescription;
+          break;
+        case 'start':
+          this.sortedByStart ? this.courses.reverse() : this.courses.sort(this._sortByStart); 
+          this.sortedByStart = !this.sortedByStart;
+          break;
+        case 'end':
+          this.sortedByEnd ? this.courses.reverse() : this.courses.sort(this._sortByEnd); 
+          this.sortedByEnd = !this.sortedByEnd;
+          break;
+      }
+    }
+  }
+
+  private _sortById(a: Course, b: Course): number {
+    if (a.id < b.id) {
+      return -1;
+    } 
+    else if (a.id > b.id) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  private _sortByTitle(a: Course, b: Course): number {
+    if (a.course.toLowerCase() < b.course.toLowerCase()) {
+      return -1;
+    } 
+    else if (a.course.toLowerCase() > b.course.toLowerCase()) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  private _sortByDescription(a: Course, b: Course): number {
+    if (a.description.toLowerCase() < b.description.toLowerCase()) {
+      return -1;
+    } 
+    else if (a.description.toLowerCase() > b.description.toLowerCase()) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  private _sortByStart(a: Course, b: Course): number {
+    if (a.start_maxile_score < b.start_maxile_score) {
+      return -1;
+    } 
+    else if (a.start_maxile_score > b.start_maxile_score) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  private _sortByEnd(a: Course, b: Course): number {
+    if (a.end_maxile_score < b.end_maxile_score) {
+      return -1;
+    } 
+    else if (a.end_maxile_score > b.end_maxile_score) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+
+  //end sort block
 
 }
