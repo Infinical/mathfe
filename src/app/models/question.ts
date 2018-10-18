dd316a140a05bfd9ae681aafc6b4808e25ef8a09
Fernando Diaz <fer9h7@gmail.com>
2018-10-18 15:42:05 -0600

    Question model

--- src/app/models/question.ts
+++ src/app/models/question.ts
@@ -1,4 +1,23 @@
 export class Question {
-  constructor() {
-  }
+
+	public answer0: string;
+	public answer0_image: string;
+	public answer1: string;
+	public answer1_image: string;
+	public answer2: string;
+	public answer2_image: string;
+	public answer3: string;
+	public answer3_image: string;
+	public correct_answer: number;
+	public difficulty_id: number;
+	public id: number;
+	public question: string;
+	public question_image: string;
+	public skill_id: number;
+	public status_id: number;
+	public type_id: number;
+
+	constructor() {
+
+	}
 }
