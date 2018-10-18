bb82120bb266b16caa23a00df0a969ea206c5322
Fernando Diaz <fer9h7@gmail.com>
2018-10-17 18:57:22 -0600

    Using material dialog for question delete

--- src/app/admin/admin-question-list/admin-question-list.component.ts
+++ src/app/admin/admin-question-list/admin-question-list.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../../environments/environment';
 import { QuestionService } from '../../services/question.service';
-import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material'; 
+import { MatPaginator, MatTableDataSource, MatSort, MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material'; 
+
+export interface DialogData { id: string }
 
 @Component({
   selector: 'ag-admin-question-list',
@@ -19,10 +21,9 @@ export class AdminQuestionListComponent implements OnInit {
   dataSource = new MatTableDataSource<any>();
   beURL = environment.apiURL;
   currentPage = 0;
-  selectedQuestion: any;
-  deleteResult: any;
+  selectedQuestion: any; 
 
-  constructor(private http: HttpClient, private questionService: QuestionService) { 
+  constructor(private http: HttpClient, private questionService: QuestionService, public dialog: MatDialog) { 
     this.onPaginateChange({pageIndex: 0});    
   }
 
@@ -46,56 +47,62 @@ export class AdminQuestionListComponent implements OnInit {
     if (dom) dom.style.display = 'none';    
   }
 
-  removeModal(){
-    let dom: any = document.querySelector('#modal-container');    
-    dom.classList.add('hidden');
-    
-    dom = document.querySelector('#overlay');
-    dom.classList.add('hidden');
-
-    //set modal default values
-    dom = document.querySelector('#deleteButton');    
-    dom.classList.remove('hidden');
-
-    dom = document.querySelector('#cancelButton');    
-    dom.innerHTML = "Cancel";
-
-    this.deleteResult = null;
-  }
-
   confirmDelete(question: any){
-    this.selectedQuestion = question;
+    let dialogRef = this.dialog.open(DialogDeleteQuestion, {
+      width: '250px',
+      data: { id: question.id }
+    });
 
-    let dom: any = document.querySelector('#modal-container');    
-    dom.classList.remove('hidden');
+    dialogRef.afterClosed().subscribe(result => {
+      this.onPaginateChange({pageIndex: this.currentPage});
+    });
+  }  
+}
 
-    dom = document.querySelector('#overlay');
-    dom.classList.remove('hidden');
+// dialog component
+@Component({
+  template: `<h2 mat-dialog-title>Delete Question</h2>
+<mat-dialog-content>Are you sure?
+<div style="margin-top: 0.5rem;" *ngIf="deleteResult && deleteResult.status ==='success'" class="alert alert-success" role="alert"> {{ deleteResult.message }} </div>
+<div style="margin-top: 0.5rem;" *ngIf="deleteResult && deleteResult.status ==='error'" class="alert alert-danger" role="alert"> {{ deleteResult.message }} </div>
+</mat-dialog-content>
+<mat-dialog-actions>
+  <button mat-button mat-dialog-close>No</button>
+  <!-- The mat-dialog-close directive optionally accepts a value as a result for the dialog. -->
+  <button mat-button (click)="onYesClick()">Yes</button>
+</mat-dialog-actions>`,
+  selector: 'dialog-delete-question'
+})
+
+export class DialogDeleteQuestion {
+
+  deleteResult: any;
+
+  constructor(
+    private questionService: QuestionService,
+    public dialogRef: MatDialogRef<DialogDeleteQuestion>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  public onNoClick(): void {
+    this.dialogRef.close();
   }
 
-  deleteQuestion(){
+  public onYesClick(): void {
 
-    this.questionService.deleteQuestion(this.selectedQuestion.id)
-      .subscribe(res => {
-
-        this.onPaginateChange({pageIndex: this.currentPage});
+    this.questionService.deleteQuestion(this.data.id)
+      .subscribe(res => {        
 
         this.deleteResult = {
           status: 'success',
           message: res["message"]
         };
 
-        let dom: any = document.querySelector('#deleteButton');    
-        dom.classList.add('hidden');
-
-        dom = document.querySelector('#cancelButton');    
-        dom.innerHTML = "Close";
-
       }, error => {
         this.deleteResult = {
           status: 'error',
-          message: error["message"]
+          message: 'Server Error'
         };
-      });
-    }
+      });    
+  }
 }
