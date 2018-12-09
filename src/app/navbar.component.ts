008322ff5e43f1ff79f72ea2a6095079e90b7d11
amitumardev89 <amitumardev89@gmail.com>
2018-12-09 21:45:06 +0530

    fix Nav bar

--- src/app/navbar.component.ts
+++ src/app/navbar.component.ts
@@ -22,13 +22,13 @@ export class NavbarComponent implements OnInit {
       }
     });
     
-    $(document).ready(function () {
-      mainNav();
-    });
+    // $(document).ready(function () {
+    //   mainNav();
+    // });
 
-    $(window).scroll(function () {
-      mainNav();
-    });
+    // $(window).scroll(function () {
+    //   mainNav();
+    // });
 
     $(document).ready(function () {
       $('.main-navigation').onePageNav({
@@ -39,20 +39,20 @@ export class NavbarComponent implements OnInit {
 
     })
 
-    function mainNav() {
-      if (matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
-        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        if (top > 40) $('.sticky-navigation').stop().animate({ "top": '0' });
+    // function mainNav() {
+    //   if (matchMedia('(min-width: 768px) and (max-width: 991px)').matches) {
+    //     var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    //     if (top > 40) $('.sticky-navigation').stop().animate({ "top": '0' });
 
-        else $('.sticky-navigation').stop().animate({ "top": '-120' });
-      }
-      if (matchMedia('(min-width: 992px), (max-width: 767px)').matches) {
-        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-        if (top > 40) $('.sticky-navigation').stop().animate({ "top": '0' });
+    //     else $('.sticky-navigation').stop().animate({ "top": '-120' });
+    //   }
+    //   if (matchMedia('(min-width: 992px), (max-width: 767px)').matches) {
+    //     var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    //     if (top > 40) $('.sticky-navigation').stop().animate({ "top": '0' });
 
-        else $('.sticky-navigation').stop().animate({ "top": '-60' });
-      }
-    }
+    //     else $('.sticky-navigation').stop().animate({ "top": '-60' });
+    //   }
+    // }
 
   }
 
