9c2966e127c98a624a91080b002a6b4aca2e86e3
amitumardev89 <amitumardev89@gmail.com>
2018-12-27 18:40:56 +0530

    Student and Course Page changes..

--- src/app/member/teach-list/teach-detail/chart/chart.component.ts
+++ src/app/member/teach-list/teach-detail/chart/chart.component.ts
@@ -8,15 +8,25 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ChartComponent implements OnInit {
   @Input() chartdata: any;
   // Doughnut
-  public doughnutChartLabels:string[] = ['Underperform','Overperform', 'On Target'];
-  public doughnutChartType:string = 'doughnut';
- 
+  public doughnutChartLabels: string[] = ['Underperform', 'Overperform', 'On Target'];
+  public doughnutChartType: string = 'doughnut';
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: [
+        'rgb(151, 37, 23)',// Underperform - Red
+        'rgb(117, 197, 47)',// Overperform - Green
+        'rgb(249, 221, 150)'// On Target - Yellow
+      ]
+    }
+  ];
+
   // events
-  public chartClicked(e:any):void {
+  public chartClicked(e: any): void {
     console.log(e);
   }
- 
-  public chartHovered(e:any):void {
+
+  public chartHovered(e: any): void {
     console.log(e);
   }
 
