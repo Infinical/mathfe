f10a9a414454cb5e3f703d461581576a7f919f6b
amitumardev89 <amitumardev89@gmail.com>
2019-01-09 13:24:40 +0530

    Add/Update Skill

--- src/app/admin/admin-skill-create/admin-skill-create.component.ts
+++ src/app/admin/admin-skill-create/admin-skill-create.component.ts
@@ -1,15 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { SkillService } from '../../services/skill.service';
 @Component({
   selector: 'ag-admin-skill-create',
   templateUrl: './admin-skill-create.component.html',
   styleUrls: ['./admin-skill-create.component.css']
 })
 export class AdminSkillCreateComponent implements OnInit {
-
-  constructor() { }
+  public status: string;
+  public message: string;
+  public selectedFile: File = null;
+  public lesson_link: string = 'images/upload.png';
+  statuses: any;
+  my_tracks = [];
+  public_tracks = [];
+  constructor(
+    private skillService: SkillService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.skillService.createSkill().subscribe(
+      data => {
+        this.statuses = data['statuses'];
+        this.my_tracks = data['my_tracks'] || [];
+        this.public_tracks = data['public_tracks'] || [];
+      },
+      error => console.log(<any>error));
   }
 
+  public createSkill(skill): void {
+    const formData: FormData = new FormData();
+    if ((skill.video){
+      if (!this.lesson_link.includes(skill.video)) {
+        formData.append('lesson_link', this.selectedFile);
+      }
+    }
+    formData.append('skill', skill.skill);
+    formData.append('description', skill.description);
+    formData.append('track_id', skill.track_id);
+    formData.append('status_id', skill.status_id);
+    this.skillService.addSkill(formData)
+      .subscribe(
+        skill => {
+          this.skillService.updateStatus = skill['message'];
+          setTimeout(() => this.skillService.updateStatus = '', 2000);
+          this.router.navigate(['/admin/skills']);
+          setTimeout(() => window.scrollTo(0, 0), 0);
+        },
+        error => {
+          console.log(<any>error);
+          this.status = 'success';
+          this.message = error['message'];
+        }
+      );
+  }
+
+  public onFileSelected(files: FileList): void {
+    this.selectedFile = files.item(0);
+    let reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.lesson_link = event.target.result;
+    }
+    reader.readAsDataURL(this.selectedFile);
+  }
 }
--- src/app/admin/admin-skill-edit/admin-skill-edit.component.ts
+++ src/app/admin/admin-skill-edit/admin-skill-edit.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
+import { SkillService } from '../../services/skill.service';
+import { Skill } from '../../models/skill';
 
 @Component({
   selector: 'ag-admin-skill-edit',
   templateUrl: './admin-skill-edit.component.html',
   styleUrls: ['./admin-skill-edit.component.css']
 })
-export class AdminSkillEditComponent implements OnInit {
+export class AdminSkillEditComponent implements OnInit, OnDestroy {
+  beURL = environment.apiURL + '/';
+  status: string;
+  message: string;
+  id: any;
+  params: any;
+  selectedFile: File = null;
+  lesson_link: string = "images/upload.png";
+  formData: FormData = new FormData();
+  statuses: any;
 
-  constructor() { }
+  skill = new Skill('id', 'skill', 'description', 'user_id', 'image', 'lesson_link', 'status_id');
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private skillService: SkillService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.params = this.activatedRoute.params.subscribe(params => this.id = params['id']);
+    this.skillService.getSkill(this.id).subscribe(
+      data => {
+        this.skill = data;
+        this.lesson_link = this.beURL + this.skill.lesson_link;
+      },
+      error => console.log(<any>error));
+
+    this.skillService.createSkill().subscribe(
+      data => {
+        this.statuses = data['statuses'];
+      },
+      error => console.log(<any>error));
+
+  }
+
+
+
+  ngOnDestroy() {
+    this.params.unsubscribe();
+  }
+
+  updateSkill(skill) {
+    this.formData.append('_method', 'PATCH');
+    if ((skill.video)){
+      if (!this.lesson_link.includes(skill.video)) {
+        this.formData.append('lesson_link', this.selectedFile);
+      }
+    }
+    this.formData.append('description', skill.description);
+    this.formData.append('skill', skill.skill);
+    this.formData.append('status_id', skill.status_id);
+    this.skillService.updateSkillWithFormData(this.formData, skill.id)
+      .subscribe(
+        skill => {
+          this.status = 'success';
+          this.message = skill['message'];
+          this.skillService.updateStatus = this.message = skill['message'];
+          setTimeout(() => this.skillService.updateStatus = '', 2000);
+          this.router.navigate(['/admin/skills']);
+          setTimeout(() => window.scrollTo(0, 0), 0);
+        },
+        error => {
+          console.log(<any>error);
+          this.status = 'success';
+          this.message = error['message'];
+        }
+      );
+  }
+
+  onFileSelected(files: FileList) {
+    this.selectedFile = files.item(0);
+    var reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.lesson_link = event.target.result;
+    }
+    reader.readAsDataURL(this.selectedFile);
   }
 
 }
