f654a84fa208d9030273e8780fbc9f9d9ee9bd9d
Pamela Lim <pamelaliusm@gmail.com>
2019-01-29 20:59:34 +0800

    ks changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,47 +5,20 @@
 import { PayPalEnvironment } from 'ngx-paypal';
 export const environment = {
   production: false,
-<<<<<<< HEAD
-  apiURL: 'http://mathapi.pamelalim.me',
-=======
-  apiURL: 'http://devapi.pamelalim.me',
->>>>>>> 49d32441904de8910b698a28ccc94713ed4aa0ba
+  apiURL: 'http://localhost',
   webAuth: {
-    clientID: 'eVJv6UFM9GVdukBWiURczRCxmb6iaUYG',
-    domain: 'pamelalim.auth0.com',
-    audience: 'https://pamelalim.auth0.com/userinfo',
-    redirectUri: 'http://localhost:4200/home',
+    clientID: 'bs3jSKz2Ewrye8dD2qRVrD0Tra2tOqHC',
+    domain: 'allgiftedllc.au.auth0.com',
+    audience: 'https://allgiftedllc.au.auth0.com/userinfo',
+    redirectUri: 'http://localhost:4200/',
     theme: {
       logo: "http://school.all-gifted.com/pluginfile.php/1/theme_lambda/logo/1472088488/newlogo.png"
     }
   },
   payPal: {
-    payPalEnvironment: PayPalEnvironment.Production,
-    productionKey: "AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R",
+    payPalEnvironment: PayPalEnvironment.Sandbox,
+    productionKey: "AcWwalMgPfGKd-sT_fHGPOf6hAnLc_i8LfhuWlGSl29pd4tR3FWi66FGJt_dI6LPcPvby0yQ1JP4UTK0",
     sandboxKey: "AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R",
-    postEnrollmentUrl: 'http://devapi.pamelalim.me/enrolments'
+    postEnrollmentUrl: 'http://mathapi.pamelalim.me/enrolments'
   }
 };
-
-
-
-// import { PayPalEnvironment } from 'ngx-paypal';
-// export const environment = {
-//   production: true,
-//   apiURL: 'http://devapi.pamelalim.me',
-//   webAuth: {
-//     clientID: 'eVJv6UFM9GVdukBWiURczRCxmb6iaUYG',
-//     domain: 'pamelalim.auth0.com',
-//     audience: 'https://pamelalim.auth0.com/userinfo',
-//     redirectUri: 'http://localhost:4200/home',
-//     theme: {
-//       logo: "http://school.all-gifted.com/pluginfile.php/1/theme_lambda/logo/1472088488/newlogo.png"
-//     }
-//   },
-//   payPal: {
-//     payPalEnvironment: PayPalEnvironment.Production,
-//     productionKey: "AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R",
-//     sandboxKey: "AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R",
-//     postEnrollmentUrl: 'http://devapi.pamelalim.me/enrolments'
-//   }
-// };
