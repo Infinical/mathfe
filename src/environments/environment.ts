acd7e145852c07e6d085a2552283946c95d6c5ea
Pamela Lim <pamelaliusm@gmail.com>
2019-02-14 23:42:59 +0800

    b4 prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,14 @@
 import { PayPalEnvironment } from 'ngx-paypal';
 export const environment = {
   production: false,
-<<<<<<< HEAD
   apiURL: 'http://localhost:8000',
-=======
-  apiURL: 'http://devapi.pamelalim.me',
->>>>>>> ecd4a03d1ea9e862bdab3389b153e66848738109
+//  apiURL: 'http://devapi.pamelalim.me',
   webAuth: {
-    clientID: 'eVJv6UFM9GVdukBWiURczRCxmb6iaUYG',
-    domain: 'pamelalim.auth0.com',
-    audience: 'https://pamelalim.auth0.com/userinfo',
+      clientID:'bs3jSKz2Ewrye8dD2qRVrD0Tra2tOqHC',
+      domain: 'allgiftedllc.au.auth0.com',
+//    clientID: 'eVJv6UFM9GVdukBWiURczRCxmb6iaUYG',
+//    domain: 'pamelalim.auth0.com',
+    audience: 'https://allgiftedllc.au.auth0.com/userinfo',
     redirectUri: 'http://localhost:4200/home',
     theme: {
       logo: "http://school.all-gifted.com/pluginfile.php/1/core_admin/logo/0x150/1535925593/logo.png"
