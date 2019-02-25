d11e4c1b8509688ed7d80371fcd2173d57cd5e58
amitumardev89 <amitumardev89@gmail.com>
2019-02-25 11:26:59 +0530

    Update user profile, and us

--- src/app/admin/admin-user-list/admin-user-list.component.ts
+++ src/app/admin/admin-user-list/admin-user-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs/Observable';
-import {User} from '../../models/user';
-import {UserService} from '../../services/user.service';
-
+import { Observable } from 'rxjs/Observable';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
+import { MatDialog } from '@angular/material';
+import { HelperService } from '../../services/helper.service';
 @Component({
   selector: 'ng-admin-user-list',
   templateUrl: './admin-user-list.component.html',
@@ -27,14 +29,18 @@ export class AdminUserListComponent implements OnInit {
     Edit: true
   }
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, public dialog: MatDialog, private helperService: HelperService) { }
 
   ngOnInit() {
+    this.bindUsers();
+  }
+  private bindUsers() {
     this.userService.getUsers().subscribe(
-	  data => {
-	    this.users = data;
-	  },
-	  error =>  console.error(<any>error));
+      data => {
+        this.users = data;
+      },
+      error => console.error(<any>error));
+
   }
   resetUpdateStatus() {
     this.userService.updateStatus = '';
@@ -42,6 +48,27 @@ export class AdminUserListComponent implements OnInit {
 
   get updateStatus(): string {
     return this.userService.updateStatus;
+  }
+
+  public openDialog(id: String): void {
+    this.dialog.open(ConfirmDialogComponent, { data: { message: "Are you sure?", title: "Delete User" } }).afterClosed().
+      subscribe(ifYes => {
+        if (ifYes) {
+          this.userService.deleteUser(id).subscribe(
+            data => {
+              this.userService.updateStatus = data['message'];
+              window.scrollTo(0, 0);
+              setTimeout(() => this.userService.updateStatus = '', 2000);
+
+            },
+            error => {
+              window.scrollTo(0, 0);
+              this.userService.updateStatus = this.helperService.ParseErrorMsg(error);
+            });
+        } else {
+          //rejected
+        }
+      });
   }
 
 }
